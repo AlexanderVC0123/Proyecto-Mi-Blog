4e45456d3613a561df8d72b6ca959cf9a1fa3a83
Alexander <david_15vc@hotmail.com>
2021-02-20 23:53:02 +0100

    Primer commit agregando todos los archivos del proyecto

---
+++ database/factories/ArticuloFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Articulo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticuloFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Articulo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'articleTitle' => $this->faker->name,
+            'description' => $this->faker->paragraph(),
+            'date' => $this->faker->date,
+        ];
+    }
+}
